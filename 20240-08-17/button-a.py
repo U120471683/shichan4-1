339fa040b2f572ffcdf8f37f2054f61e6101fb69
U120471683 <shichanliaw@gmail.com>
2024-10-16 17:43:55 +0800

    xdvzxvzx

--- 20240-08-17/button-a.py
+++ 20240-08-17/button-a.py
@@ -16,12 +16,15 @@ def user_release():
     if led.is_lit:
         message="燈是開的"
         print(message)   
-            # publish a single message
-        publish.single(topic='501教室/監視器-1', payload=message, hostname='127.0.0.1',qos=2)  
+        # publish a single message
+        #auth = {'username': 'pi', 'password': 'raspberry'}
+        publish.single(topic='501教室/監視器-1', payload=message, hostname='127.0.0.1',qos=2)
     else:
 
         message="燈是關的"        
         print(message)
+
+        #auth = {'username': 'pi', 'password': 'raspberry'}
         publish.single(topic='501教室/監視器-1', payload=message, hostname='127.0.0.1',qos=2) 
 
 if __name__ == '__main__':
