8f4f34f4e22e2e2758af78e145eaa8e15d1786be
U120471683 <shichanliaw@gmail.com>
2024-10-16 13:34:55 +0800

    dfgfgs

--- 2024-0810/button1.py
+++ 2024-0810/button1.py
@@ -4,19 +4,17 @@ from datetime import datetime
 
 def user_release():
     print("使用者按下放開")
-    led.toggle()
-    now = datetime.now()
-    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    led.toggle() #`toggle()` 方法會切換 LED 的狀態  #
+    now = datetime.now() # 取得現在時間 
+    now_str = now.strftime('%Y-%m-%d %H:%M:%S') # 轉換成字串
     print(now_str)
-    if led.is_lit:
+    if led.is_lit:  # 判斷 LED 是否亮著 # `is_lit` 屬性會回傳 LED 的狀態
         print("燈是開的")        
     else:
         print("燈是關的")
 
 if __name__ == '__main__':
-    button = Button(pin=18)
-    button.when_released = user_release
-    led = LED(pin=25) 
-    signal.pause()
-
-# End of file
+    button = Button(pin=18)  # 按鈕接在 GPIO 18 腳位 # 建立 Button 物件
+    button.when_released = user_release # 按鈕放開時執行 user_release 函式
+    led = LED(pin=25) # LED 接在 GPIO 25 腳位 # 建立 LED 物件
+    signal.pause() # 等待訊號，程式不會結束 
